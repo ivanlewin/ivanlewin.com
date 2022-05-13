964ba422fa8f11515adadaa9f64e02bf6c5eb0c6
Iván Lewin <57846638+ivanlewin@users.noreply.github.com>
2022-05-13 00:24:29 -0300

    temp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 
 import { appWithI18Next } from 'ni18n';
-import { ni18nConfig } from '../ni18n.config'
+import { ni18nConfig } from '../ni18n.config';
+import { ThemeProvider } from '@mui/material';
+import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default appWithI18Next(MyApp, ni18nConfig);
