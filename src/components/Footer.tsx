fcf46940397d18fe8970007a348b7867c8e3cbe2
Iván Lewin <57846638+ivanlewin@users.noreply.github.com>
2026-06-11 03:16:07 -0300

    removed email from Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -39,9 +39,6 @@ const Footer = () => {
             gap: theme.spacing(2.5),
           }}
         >
-          <a target="_blank" href="mailto:ivanlewin.trabajo@gmail.com" rel="noopener noreferrer">
-            <Icon src="/pictures/Email.svg" title="Email" alt="Email icon" />
-          </a>
           <a target="_blank" href="https://www.linkedin.com/in/ivanlewin" rel="noopener noreferrer">
             <Icon src="/pictures/LinkedIn.svg" title="LinkedIn" alt="LinkedIn Logo" />
           </a>
